4d1829132cf67c5c7aa05e9227cfcf198945822d
ivan-22001 <ivan.sanguinetti@gmail.com>
2025-03-26 15:53:40 -0300

    Codigo mejorado

--- practica0/plataformaCargaUsuarios.c
+++ practica0/plataformaCargaUsuarios.c
@@ -3,11 +3,15 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-struct Usuario{
-    char nombre[100];
+typedef struct {
+    char *nombre;
     int edad;
-    char CUIT[11];
-};
+    char CUIT[12];
+}Usuario;
+
+Usuario* user = NULL;
+Usuario** usuarios = NULL;
+int size;
 
 bool cuitValido(char* cuit){
     if(strlen(cuit) != 11) return false;
@@ -22,44 +26,96 @@ bool cuitValido(char* cuit){
     return (cuit[10] - '0') == count; 
 }
 
-int main(){
+void liberarUsuario(){
+    if (user != NULL) {
+        free(user->nombre);
+        free(user);
+        user = NULL;
+    }
+}
 
-    struct Usuario* user = NULL;
+void append(Usuario* usuario){
+    for(int i = 0; i < size; i++){
+        if(usuarios[i] == NULL){
+            usuarios[i] = usuario;
+            return;
+        }
+    } 
+}
+void agregarInformacionUsuario(){
+
+    if(!usuarios){
+        usuarios = (Usuario**)malloc(sizeof(Usuario*));
+        size = 1;
+    }
+
+    user = (Usuario*)malloc(sizeof(Usuario));
+    char buffer[100];
+
+    printf("Ingrese nombre de persona:\n > ");
+    scanf("%s",buffer);
+    user->nombre = strdup(buffer);
+            
+    printf("Ingrese edad:\n > ");
+    scanf("%d",&user->edad);
+  
+
+    printf("Ingrese cuit:\n > ");
+    scanf("%11s",buffer);
+    strcpy(user->CUIT,buffer);
+
+    if((user->edad > 17 && user->edad < 100) && cuitValido(user->CUIT)){
+        if(sizeof(usuarios)+1 > size){
+            realloc(usuarios, size*2);
+            size *= 2;
+        }
+        append(user);
+        printf("Su información fue cargada con éxito!\n");
+    }
+    else{
+        printf("La información cargada es invalida, por favor, intente de nuevo\n");
+    }    
+}
+
+void verInformacion(){
+    if(!usuarios)
+        printf("¡Ningún usuario fue cargado a la plataforma todavía!\n");
+    else{   
+        int i = 0;
+        while(usuarios[i] != NULL){
+            printf("nombre: %s, edad: %d, cuit: %s \n",usuarios[i] ->nombre,usuarios[i] ->edad,usuarios[i] ->CUIT);
+            i++;
+        }
+    }
+}
+
+void buscarInfo(char* cuit){
+    for(int i = 0; i < size && usuarios[i] != NULL; i++){
+        if(strcmp(cuit,usuarios[i]->CUIT)==0){
+            printf("nombre: %s, edad: %d, cuit: %s \n",usuarios[i] ->nombre,usuarios[i] ->edad,usuarios[i] ->CUIT);
+            return;
+        }
+    }
+    printf("No existe el usuario\n");
+}
+
+int main(){
+    
     char input[10000];
 
     while(1){
         printf("> ");
         scanf("%99s",input);
         if(strcmp(input,"agregarInformacionUsuario")==0){
-            char nombre[100];
-            int edad;
-            char CUIT[11];
-            printf("Ingrese nombre de persona:\n > ");
-            scanf("%s",nombre);
-            
-            printf("Ingrese edad:\n > ");
-            scanf("%d",&edad);
-            while (getchar() != '\n');
-
-            printf("Ingrese cuit:\n > ");
-            scanf("%s",CUIT);
-            if((edad > 17 && edad < 100) && cuitValido(CUIT)){
-                user = malloc(sizeof(struct Usuario));
-                strcpy(user->CUIT,CUIT);
-                user->edad = edad;
-                strcpy(user->nombre,nombre);
-                printf("Su información fue cargada con éxito!\n");
-            }
-            else{
-                printf("La información cargada es invalida, por favor, intente de nuevo\n");
-            }
+            agregarInformacionUsuario();
         }
         else if(strcmp(input,"verInformacionUsuario") == 0){
-            if(!user)
-                printf("¡Ningún usuario fue cargado a la plataforma todavía!\n");
-            else{
-                printf("nombre: %99s, edad: %d, cuit: %s \n",user->nombre,user->edad,user->CUIT);
-            }
+            verInformacion();
+        }
+        else if(strcmp(input,"buscarInformacion") == 0){
+            printf("Ingrese el cuit a buscar: \n > ");
+            scanf("%s", input);
+            buscarInfo(input);
         }
         else 
             printf("Input invalido, ingrese devuelta\n");
